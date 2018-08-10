e5100f1018b53ee5c3e1ede46a150b922ea39e7c
Taylor Beeston <beeston.taylor@gmail.com>
2018-08-10 02:44:13 +0000

    Add user following

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -19,4 +19,11 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
       assert_match micropost.content, response.body
     end
   end
+  
+  test "stats display" do
+    log_in_as @user
+    get root_url
+    assert_template 'static_pages/home'
+    assert_select 'div.stats'
+  end
 end
