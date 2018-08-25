be4269513e3de0e70bdcb6b77ffed97354acaa22
TaylorBeeston <beeston.taylor@gmail.com>
2018-08-25 14:07:54 -0700

    Bugfixes and add helper for github link

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,5 @@
 module StaticPagesHelper
+  def github_link
+    "https://github.com/TaylorBeeston/I-Love-Andi"
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = '05b5cb5f63b842d093539332212231ef.vfs.cloud9.us-east-2.amazonaws.com'
+  host = 'localhost:3000'
   config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   config.action_mailer.perform_caching = false
